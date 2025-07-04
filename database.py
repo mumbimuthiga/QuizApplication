4095dd94002b768998fb50927f8e776883d717b4
mumbimuthiga <veronicmuthiga@gmail.com>
2025-07-04 06:50:45 +0300

    crud operations

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-URL_DATABASE='postgresql://postgres:password@localhost:5432/QuizApplication'
+
+URL_DATABASE='postgresql://postgres:mumbi%40123@localhost:5433/QuizApplication'
 
 engine=create_engine(URL_DATABASE)
 SessionLocal=sessionmaker(autocommit=False ,autoflush=False,bind=engine)
-
 Base=declarative_base()
--- main.py
+++ main.py
@@ -26,3 +26,33 @@ def get_db():
 
 db_dependency=Annotated[Session,Depends(get_db)]
 
+@app.post("/questions")
+async def create_questions(question:QuestionBase,db:db_dependency):
+    db_question=models.Questions(question_text=question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+
+    for choice in question.choices:
+        db_choice=models.Choices(choice_text=choice.choice_text,is_correct=choice.is_correct,question_id=db_question.id)
+        db.add(db_choice)
+    db.commit()
+
+
+@app.get("/questions/{question_id}")
+async def read_questions(question_id:int ,db:db_dependency):
+    result=db.query(models.Questions).filter(models.Questions.id==question_id).first()
+    if not result:
+       raise HTTPException(status_code=400,detail="Question is not found")
+    return result
+    
+@app.get("/choices/{question_id}")
+
+async def read_choices(question_id:int,db:db_dependency):
+    result=db.query(models.Choices).filter(models.Choices.question_id==question_id).all()
+    if not result:
+        raise HTTPException(status_code=400,detail="Choices not Found")
+    return result
+
+
+
